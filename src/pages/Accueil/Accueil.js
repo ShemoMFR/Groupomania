ec898667a02ed9e225d98d26a1fe7563e538937d
Shemo <moshe.frydmann@gmail.com>
2022-02-23 16:07:58 +0200

    router Set

---
+++ src/pages/Accueil/Accueil.js
@@ -0,0 +1,13 @@
+/* LIBRAIRIES */
+import React from 'react';
+
+/* CSS */
+import './Accueil.css'; 
+
+const Accueil = () => {
+    return (
+        <div>Accueil</div>
+    )
+}
+
+export default Accueil
